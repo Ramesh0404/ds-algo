179e28cb8a80935bfcb56824c6db1264bcddd577
ramesh <ramesh.v@bluetickconsultants.com>
2022-06-08 23:34:51 +0530

    checked with test cases

--- first-last.py
+++ first-last.py
@@ -1,34 +1,40 @@
 class Solution:
     def first(self,nums,target):
-        left=0
-        right=len(nums)-1
+        low_index=0
+        high_index=len(nums)-1
         first=-1
-        while left<=right:
-            middle=left+(right-left)//2
-            if target==nums[middle]:
-                first=middle
-                right=middle-1
-            elif target<nums[middle]:
-                right=middle-1
+        while low_index<=high_index:
+            mid_index=low_index+(high_index-low_index)//2
+            if target==nums[mid_index]:
+                first=mid_index
+                high_index=mid_index-1
+            elif target<nums[mid_index]:
+                high_index=mid_index-1
             else:
-                left=middle+1
+                low_index=mid_index+1
         return first
     
     def last(self,nums,target):
-        left=0
-        right=len(nums)-1
+        low_index=0
+        high_index=len(nums)-1
         last=-1
-        while left<=right:
-            middle=left+(right-left)//2
-            if target==nums[middle]:
-                last=middle
-                left=middle+1
-            elif target<nums[middle]:
-                right=middle-1
+        while low_index<=high_index:
+            mid_index=low_index+(high_index-low_index)//2
+            if target==nums[mid_index]:
+                last=mid_index
+                low_index=mid_index+1
+            elif target<nums[mid_index]:
+                high_index=mid_index-1
             else:
-                left=middle+1
+                low_index=mid_index+1
         return last
     
     def searchRange(self,nums, target):
         return [self.first(nums, target), self.last(nums, target)]
-        
+
+nums=[5,7,7,8,8,10]
+target=8
+
+r=Solution()
+s=r.searchRange(nums, target)
+print(s)
