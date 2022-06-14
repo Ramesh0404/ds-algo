fda821013e0485825a0177d2bc4ee6fd49ecaa59
ramesh <ramesh.v@bluetickconsultants.com>
2022-06-14 14:06:09 +0530

    added the longest increasing subsequence solution

--- longest-subsequence.py
+++ longest-subsequence.py
@@ -1,14 +1,16 @@
-def lis(arr):
-    n = len(arr)
-    lis = [1]*n
-    for i in range(1, n):
-        for j in range(0, i):
-            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
-                lis[i] = lis[j]+1
-    maximum = 0
-    for i in range(n):
-        maximum = max(maximum, lis[i])
-    return maximum
+class Solution:
+    def lengthOfLIS(self, nums):
+        n=len(nums)
+        l = [1]*n
+        for i in range(1,n):
+            for j in range(0,i):
+                if nums[i]>nums[j] and l[i]<l[j]+1:
+                    l[i]=l[j]+1
+        maximum=0
+        for i in range(n):  
+            maximum=max(maximum, l[i])
+        return maximum
 
-arr = [7,7,7,7,7,7,7]
-print(lis(arr))
+a = [0,1,0,3,2,3]
+r=Solution()
+print(r.lengthOfLIS(a))
