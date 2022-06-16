f94dff9a8e92f51a72d5f39dabcb291731176a84
ramesh <ramesh.v@bluetickconsultants.com>
2022-06-16 13:18:45 +0530

    added find all duplicates solution

---
+++ find-duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicates(self, nums):
+        dup_check={}
+        dup=[]
+        for i in nums:
+            if i in dup_check:
+                dup.append(i)
+            else:
+                dup_check[i]=i
+        return dup
+
+a=[4,3,2,7,8,2,3,1]
+r=Solution()
+print(r.findDuplicates(a))
